ae2a413371c7eeaf1d1c6b4b12df7b989628c9e7
reloZid <relozid@logixisland.ch>
2015-10-12 20:34:13 +0200

    reverted to old man page colors

--- bash/update.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -O git-completion.bash
-wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -O git-prompt.sh
