d6c576ad7e26243d26d2a9f2baec573720655d4a
reloZid <relozid@logixisland.ch>
2015-10-17 15:55:57 +0200

    corrected font URL

--- setup.sh
+++ setup.sh
@@ -16,5 +16,5 @@ ln -nfsv $CWD/vim $HOME/.vim
 vim +BundleInstall +qall
 
 # Powerline Font
-wget https://github.com/powerline/fonts/blob/master/UbuntuMono/Ubuntu%20Mono%20derivative%20Powerline.ttf -O ~/font.ttf 2> /dev/null
+wget https://github.com/powerline/fonts/raw/master/UbuntuMono/Ubuntu%20Mono%20derivative%20Powerline.ttf -O ~/font.ttf 2> /dev/null
 echo "install ~/font.ttf and set it as terminal font to enable powerline symbols in vim"
