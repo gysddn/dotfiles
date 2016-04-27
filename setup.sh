386e6053e7cc73b97d6df35b786000e43dd61be0
reloZid <relozid@logixisland.ch>
2016-04-27 13:42:28 +0200

    made a mistake with symlink paths

--- setup.sh
+++ setup.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-cd "$(dirname "${BASH_SOURCE[0]}")"
+script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+cd $script_dir
 
 # Init submodules
 git submodule update --init --recursive
 
 # Bash config
-ln -nfsv bash/bashrc $HOME/.bashrc
+ln -sfv $script_dir/bash/bashrc $HOME/.bashrc
 
 # Tmux config
-ln -nfsv tmux/tmux.conf $HOME/.tmux.conf
+ln -sfv $script_dir/tmux/tmux.conf $HOME/.tmux.conf
 
 # Vim config
-ln -nfsv vim/vimrc $HOME/.vimrc
-ln -nfsv vim $HOME/.vim
+ln -sfv $script_dir/vim/vimrc $HOME/.vimrc
+ln -sfv $script_dir/vim $HOME/.vim
 
 # Install vim plugins
 vim +BundleInstall +qall
-
