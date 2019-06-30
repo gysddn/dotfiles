20345af0cf6d7f5b50f780974e303a4cdd3aba52
gysddn <gysddn@gmail.com>
2019-06-30 14:50:25 +0300

    xorg and i3 added

--- setup.sh
+++ setup.sh
@@ -16,5 +16,16 @@ ln -sfv $script_dir/tmux/tmux.conf $HOME/.tmux.conf
 ln -sfv $script_dir/vim/vimrc $HOME/.vimrc
 ln -sfv $script_dir/vim $HOME/.vim
 
+# Xorg config(s)
+ln -sfv $script_dir/xorg/xinitrc $HOME/.xinitrc
+ln -sfv $script_dir/xorg/Xmodmap $HOME/.Xmodmap
+ln -sfv $script_dir/xorg/Xresources $HOME/.Xresources
+
+# i3 config
+if [ ! -d $HOME/.config/i3 ]; then
+	mkdir -p $HOME/.config/i3
+fi
+ln -sfv $script_dir/i3/config $HOME/.config/i3/config
+
 # Install vim plugins
 vim +BundleInstall +qall
