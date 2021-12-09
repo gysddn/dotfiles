e5ee386495f7795e76745bd80c33959c1eae317f
Gıyaseddin <gysddn@gmail.com>
2021-12-09 13:12:19 +0300

    completely migrate to lua config, use packer

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,205 +1,93 @@
 ----------------------------- HELPERS -----------------------------
---plugins = require'plugins'
+plugins = require'plugins'
 
 local cmd = vim.cmd  -- to run vim commands
 local fn  = vim.fn   -- to run vim functions
 local g   = vim.g    -- global variable table
--- o -> options, b -> buffer options, w -> window options
-local scopes = {o = vim.o, b = vim.bo, w = vim.wo}
-
-local function opt(scope, key, value)
-  scopes[scope][key] = value
-  if scope ~= 'o' then scopes['o'][key] = value end
-end
 
 local function map(mode, lhs, rhs, opts)
   local options = {noremap = true}
   if opts then options = vim.tbl_extend('force', options, opts) end
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
-
-local fn = vim.fn
-
-local install_path = fn.stdpath('data') .. '/site/pack/paqs/start/paq-nvim'
-
-if fn.empty(fn.glob(install_path)) > 0 then
-  fn.system({'git', 'clone', '--depth=1', 'https://github.com/savq/paq-nvim.git', install_path})
-end
-
-require "paq" {
-  "savq/paq-nvim";                  -- Let Paq manage itself
-
-  'sainnhe/gruvbox-material';
-  'marko-cerovac/material.nvim';
-
-  {'nvim-treesitter/nvim-treesitter',run = ':TSUpdate'};
-
-  'sainnhe/sonokai';
-  'savq/melange';
-
-  --lsp
-  'neovim/nvim-lspconfig';
-
-  'L3MON4D3/LuaSnip';
-  'hrsh7th/cmp-nvim-lsp';
-  'hrsh7th/cmp-buffer';
-  'hrsh7th/nvim-cmp';
-
-
-  -- Show git statuf of lines
-  'airblade/vim-gitgutter';
-
-  -- Commenting quick
-  'tpope/vim-commentary';
-
-  -- surround a block with (, {, [ etc.
-  'tpope/vim-surround';
-
-  -- Let's stick with NERDTree 'til telescope comes out
-  'preservim/nerdtree';
-
-  {'junegunn/fzf', run = fn['fzf#install']};
-  'junegunn/fzf.vim';
-
-  'rhysd/vim-llvm';
-  'kyazdani42/nvim-web-devicons';
-  'nvim-lualine/lualine.nvim';
-  'alvarosevilla95/luatab.nvim';
-}
-
 ----------------------------- OPTIONS -----------------------------
 
-vim.g.bufferline = { 
-	icons = false,
-}
-
-cmd('colorscheme melange')
--- cmd('match Error /\\s\\+$/')
--- cmd('match SpecialChar /$/')
---waiting for a good patch
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
+cmd('colorscheme material')
 
 local indent = 2
-opt('o','termguicolors',true)
-opt('o','ignorecase',true)
-opt('o','list',true)
-opt('o','expandtab',true)
-opt('w','number',true)
-opt('w','relativenumber',true)
-opt('o','undofile',true)
-opt('w','wrap',false)
+--- Indentation
+vim.opt.tabstop = indent
+vim.opt.shiftwidth = indent
 
-opt('o','updatetime',100)
-opt('o','cmdheight',3)
-opt('o','scrolloff',10)
-opt('o','showtabline',2)
+vim.opt.termguicolors = true
+--- Ignore case in a pattern
+vim.opt.ignorecase = true
+--- Show the tab, trailing space etc. characters, modified by 'listchars'
+vim.opt.list = true
+--- Use spaces as tabs
+vim.opt.expandtab = true
+--- Line numbers and relative position
+vim.opt.number = true
+vim.opt.relativenumber = true
+--- Hold undo tree between sessions
+vim.opt.undofile = true
+--- Don't wrap lines
+vim.opt.wrap = false
 
-opt('o','colorcolumn','120')
-opt('o','mouse','a')
-opt('o','iskeyword','@,48-57,192-255') --no underscores(_), you can do more with this
-opt('o','wildmode','longest:full')
-opt('o','completeopt','menu,menuone,noselect')
-opt('o','inccommand','nosplit')
-opt('o','listchars','eol:↵,trail:~,tab:>-,nbsp:␣')
+--- Update swap file time interval
+-- vim.opt.updatetime = 100
+vim.opt.cmdheight = 3
+--- Scroll offset, how many lines will be above and below cursor position
+vim.opt.scrolloff = 10
+--- Always show tabline
+vim.opt.showtabline = 2
+
+--- Show a column guide at 120th
+vim.opt.colorcolumn = '120'
+--- Enable mouse in "all" modes
+vim.opt.mouse = 'a'
+--- Define what keyword is for movement commands, _ is delibarately removed
+vim.opt.iskeyword = '@,48-57,192-255'
+--- Wildmode completion. list all matches, complete first match
+vim.opt.wildmode = 'list:full'
+--- Autocompletion menu modes
+vim.opt.completeopt = 'menu,menuone,noselect,noinsert'
+--- Show the effect of command in current window
+vim.opt.inccommand = 'nosplit'
+--- char options for 'list'
+vim.opt.listchars = 'eol:↵,trail:~,tab:>-,nbsp:␣'
 
 
 ----------------------------- MAPPINGS -----------------------------
 
--- Editing and sourcing config
-map('n', '<leader>ec', '<Cmd>tabnew $MYVIMRC<CR>')
+--- Editing and sourcing config
+map('n', '<leader>ec', '<Cmd>edit $MYVIMRC<CR>')
 map('n', '<leader>sc', '<Cmd>luafile $MYVIMRC<CR>')
 
--- Clap mappings
-map('n', '<leader>cb', '<Cmd>Clap buffers<CR>')
-map('n', '<leader>cl', '<Cmd>Clap blines<CR>')
-map('n', '<leader>cf', '<Cmd>Clap files<CR>')
-map('n', '<leader>cc', '<Cmd>Clap colors<CR>')
-
--- Clap mappings
-map('n', '<space>ff', "<Cmd>lua require('telescope.builtin').find_files()<cr>")
-
--- NERDTree mappings
+--- NERDTree mappings
 map('n', '<leader>nt', '<Cmd>NERDTreeToggle<CR>')
 map('n', '<leader>nf', '<Cmd>NERDTreeFocus<CR>')
 map('n', '<leader>nc', '<Cmd>NERDTreeCWD<CR>')
 map('n', '<leader>nr', '<Cmd>NERDTreeRefreshRoot<CR>')
 
--- <Space>+ mappings
+--- <Space>+ mappings
 map('n', '<Space>q', '<Cmd>q<CR>')
 map('n', '<Space>m', ':Marks<CR>')
 map('n', '<Space>b', ':Buffers<CR>')
 
--- Split navigation
+--- Split navigation
 map('n', '<C-j>', '<C-w>j')
 map('n', '<C-k>', '<C-w>k')
 map('n', '<C-h>', '<C-w>h')
 map('n', '<C-l>', '<C-w>l')
 
--- Tab navigation
+--- Tab navigation
 map('n', '<A-h>', ':tabprevious<CR>')
 map('n', '<A-l>', ':tabnext<CR>')
 map('n', '<A-n>', ':tabnew<CR>')
 map('n', '<A-q>', ':tabclose<CR>')
 
--- Fuzzy Finder
+--- Fuzzy Finder
 map('n', '<C-p>', ':Files<CR>')
+--- Search and highlight but don't jump
 -- map('n', '*', '*``')
-
--- Search and highlight but don't jump
-
-local cmp = require'cmp'
-cmp.setup({
-    snippet = {
-      expand = function(args)
-        -- For `luasnip` user.
-        require('luasnip').lsp_expand(args.body)
-      end,
-    },
-    mapping = {
-      ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      ['<C-Space>'] = cmp.mapping.complete(),
-      ['<C-e>'] = cmp.mapping.close(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }),
-    },
-    sources = {
-      { name = 'nvim_lsp' },
-      { name = 'buffer' },
-      { name = 'luasnip' },
-    }
-  })
-
-
-vim.g.material_style = "darker"
-
------------------------------ LSP CONF -----------------------------
-require'lspconfig'.rust_analyzer.setup{
-  capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-}
--- require'lspconfig'.clangd.setup{
---   capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
--- }
-require'lspconfig'.pyright.setup{}
-require'lspconfig'.lemminx.setup{
-    cmd = { "/home/chilly/.local/opt/lemminx/lemminx" };
-    ...
-}
-
-require'nvim-treesitter.configs'.setup {
-  highlight = {
-    enable = true,
-  }
-}
-
-
------------------------------ PLUGIN CONF -----------------------------
-
---require'myconfig'.treesitter_config()
-require('lualine').setup()
-require('luatab').setup{}
-
--- Profiling
--- cmd("profile start ~/vimprofile")
--- cmd("profile file *")
--- cmd("profile func *")
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,53 +1,128 @@
-vim.cmd [[packadd packer.nvim]]
-
-function startup()
+return require('packer').startup(function()
   use 'wbthomason/packer.nvim'
 
-  -- Quck peak of registers
-  --use 'junegunn/vim-peekaboo'
-
-  -- Themes...
+  --- Colorschemes
   use 'sainnhe/gruvbox-material'
-
   use 'sainnhe/edge'
-
   use {
   'pineapplegiant/spaceduck', branch='main'
   }
+ use {
+   'marko-cerovac/material.nvim'
+ }
+ use 'sainnhe/sonokai'
+ use 'savq/melange'
 
-  -- Auto completion
-  -- use 'neoclide/coc.nvim', {'branch': 'release'}
+  --- Quck peak at registers
+  use 'junegunn/vim-peekaboo'
 
-  --use 'dhruvasagar/vim-table-mode'
+  --- Better table editing
+  use 'dhruvasagar/vim-table-mode'
 
   -- Show git statuf of lines
   use 'airblade/vim-gitgutter'
 
-  --use 'wellle/context.vim'
+  --- Show context of scopes (functions, closures, classes etc.)
+  use 'wellle/context.vim'
 
-  -- Commenting quick
+  --- Commenting quick
   use 'tpope/vim-commentary'
 
-  -- Better syntax highlight
-  use { 'nvim-treesitter/nvim-treesitter'}
-  --use 'sheerun/vim-polyglot' -- damn slow
+  --- Better syntax highlight
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate',
+    config = function() 
+      require'nvim-treesitter.configs'.setup {
+        highlight = {
+          enable = true,
+        }
+      }
+    end
+  }
 
-  -- surround a block with (, {, [ etc.
-  use 'tpope/vim-surround'
+  --- surround a block with (, {, [ etc.
+  use {
+    'tpope/vim-surround',
+    -- config = function() print("sdlkgj") end
+  }
 
+ --- LSP
+ use {
+   'neovim/nvim-lspconfig',
+   config = function() 
+     -- require'lspconfig'.rust_analyzer.setup{
+     --   capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+     -- }
+     require'lspconfig'.clangd.setup{
+       capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+     }
+     -- require'lspconfig'.pyright.setup{}
+     -- require'lspconfig'.lemminx.setup{
+     --     cmd = { "/home/chilly/.local/opt/lemminx/lemminx" };
+     --     ...
+     -- }
+   end
+ }
 
-  --use 'norcalli/typeracer.nvim'
-  --use 'nvim-treesitter/nvim-treesitter'
-
-  use 'neovim/nvim-lspconfig'
-  --use 'liuchengxu/vim-clap', { 'do': ':Clap install-binary!' }
-
-  --use 'Shougo/defx.nvim', {'do': 'UpdateRemoteuseins'}
-
-  -- Let's stick with NERDTree 'til telescope comes out
+  -- Simple file explorer
   use 'preservim/nerdtree'
 
-end
+  --- Snippet provider, completion etc. with LSP integration
+  use {
+    'hrsh7th/nvim-cmp',
+    requires = {
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'L3MON4D3/LuaSnip',
+    },
+    config = function()
+      local cmp = require'cmp'
+      cmp.setup({
+          snippet = {
+            expand = function(args)
+              require('luasnip').lsp_expand(args.body)
+            end,
+          },
+          mapping = {
+            ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+            ['<C-f>'] = cmp.mapping.scroll_docs(4),
+            ['<C-Space>'] = cmp.mapping.complete(),
+            ['<C-e>'] = cmp.mapping.close(),
+            ['<CR>'] = cmp.mapping.confirm({ select = true }),
+          },
+          sources = {
+            { name = 'nvim_lsp' },
+            { name = 'buffer' },
+            { name = 'luasnip' },
+          }
+        })
+    end
+  }
 
-return require('packer').startup(startup)
+  --- Fuzzy finder/picker
+  use {
+    'junegunn/fzf',
+    requires = 'junegunn/fzf.vim',
+    run = vim.fn['fzf#install'],
+  }
 
+  --- LLVM syntax support
+  use 'rhysd/vim-llvm'
+
+  --- Icon pack
+  use 'kyazdani42/nvim-web-devicons'
+
+  --- Status line for nvim
+  use {
+    'nvim-lualine/lualine.nvim',
+    config = function() require('lualine').setup() end
+  }
+
+  --- New tabline for nvim
+  use {
+    'alvarosevilla95/luatab.nvim',
+    config = function() require('luatab').setup() end
+  }
+
+end)
