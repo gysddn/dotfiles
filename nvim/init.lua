9524b5987bb18446ff1b1bc5e5e7281a6edf2940
Gıyaseddin <gysddn@gmail.com>
2022-02-03 16:06:12 +0300

    update config

--- nvim/init.lua
+++ nvim/init.lua
@@ -12,6 +12,7 @@ local function map(mode, lhs, rhs, opts)
 end
 ----------------------------- OPTIONS -----------------------------
 
+vim.g.material_style = "oceanic"
 cmd('colorscheme material')
 
 local indent = 2
@@ -47,9 +48,9 @@ vim.opt.colorcolumn = '120'
 --- Enable mouse in "all" modes
 vim.opt.mouse = 'a'
 --- Define what keyword is for movement commands, _ is delibarately removed
-vim.opt.iskeyword = '@,48-57,192-255'
+vim.opt.iskeyword = '@,48-57,192-255,_'
 --- Wildmode completion. list all matches, complete first match
-vim.opt.wildmode = 'list:full'
+vim.opt.wildmode = 'longest:full'
 --- Autocompletion menu modes
 vim.opt.completeopt = 'menu,menuone,noselect,noinsert'
 --- Show the effect of command in current window
@@ -89,5 +90,25 @@ map('n', '<A-q>', ':tabclose<CR>')
 
 --- Fuzzy Finder
 map('n', '<C-p>', ':Files<CR>')
+map('n', '?', ':Lines<CR>')
 --- Search and highlight but don't jump
 -- map('n', '*', '*``')
+
+--- LSP mappings
+map('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>')
+map('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
+map('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>')
+map('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
+-- map('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>')
+map('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>')
+map('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>')
+map('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>')
+map('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
+map('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>')
+map('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>')
+map('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>')
+map('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>')
+map('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
+map('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>')
+map('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>')
+map('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>')
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -8,9 +8,16 @@ return require('packer').startup(function()
   'pineapplegiant/spaceduck', branch='main'
   }
  use {
-   'marko-cerovac/material.nvim'
+   'marko-cerovac/material.nvim',
+   config = function()
+     require('material').setup({
+      contrast = false,
+    })
+   end
  }
- use 'sainnhe/sonokai'
+ use {
+   'sainnhe/sonokai',
+ }
  use 'savq/melange'
 
   --- Quck peak at registers
@@ -23,7 +30,7 @@ return require('packer').startup(function()
   use 'airblade/vim-gitgutter'
 
   --- Show context of scopes (functions, closures, classes etc.)
-  use 'wellle/context.vim'
+  -- use 'wellle/context.vim'
 
   --- Commenting quick
   use 'tpope/vim-commentary'
@@ -55,6 +62,9 @@ return require('packer').startup(function()
      --   capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
      -- }
      require'lspconfig'.clangd.setup{
+       capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+     }
+     require'lspconfig'.cmake.setup{
        capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
      }
      -- require'lspconfig'.pyright.setup{}
@@ -125,4 +135,30 @@ return require('packer').startup(function()
     config = function() require('luatab').setup() end
   }
 
+  --- Fancy UI stuff
+  use 'stevearc/dressing.nvim'
+
+  --- Quick navigaiton in a line
+  use 'unblevable/quick-scope'
+
+  --- Session Manager
+  use {
+    'Shatur/neovim-session-manager',
+    requires = {
+      'nvim-lua/plenary.nvim',
+    },
+    config = function()
+      local Path = require('plenary.path')
+      require('session_manager').setup({
+        sessions_dir = Path:new(vim.fn.stdpath('data'), 'sessions'), -- The directory where the session files will be saved.
+        path_replacer = '__', -- The character to which the path separator will be replaced for session files.
+        colon_replacer = '++', -- The character to which the colon symbol will be replaced for session files.
+        autoload_mode = require('session_manager.config').AutoloadMode.CurrentDir, -- Define what to do when Neovim is started without arguments. Possible values: Disabled, CurrentDir, LastSession
+        autosave_last_session = true, -- Automatically save last session on exit.
+        autosave_ignore_not_normal = true, -- Plugin will not save a session when no writable and listed buffers are opened.
+        autosave_only_in_session = false, -- Always autosaves session. If true, only autosaves after a session is active.
+      })
+    end,
+    disable = true,
+  }
 end)
